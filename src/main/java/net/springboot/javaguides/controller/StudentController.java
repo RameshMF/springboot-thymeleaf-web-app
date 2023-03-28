d360a3979f36c7fc31a6526231de4f621bae7e5b
Ramesh Fadatare <ramesh24fadatare@gmail.com>
2023-03-28 08:28:16 +0530

    upgraded to spring boot 3

--- src/main/java/net/springboot/javaguides/controller/StudentController.java
+++ src/main/java/net/springboot/javaguides/controller/StudentController.java
@@ -1,6 +1,6 @@
 package net.springboot.javaguides.controller;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
